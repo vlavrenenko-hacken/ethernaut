96ec0c5c9a177b3ae7560f277a968515dd325800
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-08-26 19:34:23 +0300

    Add sol 0.6.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.6.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
         version: "0.8.13",
         settings: {
           optimizer: {
